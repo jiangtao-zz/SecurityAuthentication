913f81cebed1f2e33d12b68b6bff275030e4933d
Tao <jiangtao.hu@bljc.com>
2010-10-20 17:11:27 -0400

    Move janrain into iframe

--- Security/Janrain/JanrainLoginHandler.cs
+++ Security/Janrain/JanrainLoginHandler.cs
@@ -71,80 +71,154 @@ namespace SecurityAuthentication
         public void ProcessRequest(HttpContext context)
         {
             HttpRequest request = context.Request;
-            HttpResponse response = context.Response;
 
-            if (request.QueryString["mode"] == "login")
+            switch (request.QueryString["mode"])
             {
-                string apiKey = JanrainSetting.ApiKey;
-                const string paramToken = "token";
-
-                //Check the token postback from Janrain Server
-                String returnUrl = request.QueryString["originate"];
-
-                // Get the login token passed back from the RPX authentication service
-                string loginToken = request.Form[paramToken];
-
-                if (String.IsNullOrEmpty(loginToken))
-                {
-                    response.Redirect(JanrainSetting.HomeUrl);
-                    return;
-                }
-
-                // Create an RPX wrapper to get the user's data
-                Rpx rpx = new Rpx(apiKey, JanrainSetting.ApplicationDomain);
-
-
-                //try
-                //{
-                    // Get the user's details
-                    XmlElement authInfo = rpx.AuthInfo(loginToken);
-
-
-                    // Log the user in
-                    this.loginUser(authInfo, response, returnUrl);
-                //}
-                //catch (SystemException ex)
-                //{
-                //    StringBuilder errorHtml = new StringBuilder();
-                //    errorHtml.AppendLine("Failed to login through partner site.");
-                //    errorHtml.AppendLine("<span style='display:none'>"+ex.Message+"</span>");
-                //    response.Write(errorHtml.ToString());
-                //}
-
-            }
-            else
-            {
-                //Add javascript into current login page
-                StringBuilder janrainScript = new StringBuilder();
-                String returnUrl = request.QueryString["ReturnUrl"];
-
-                janrainScript.AppendLine("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
-                janrainScript.AppendLine("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
-                janrainScript.AppendLine("<head>");
-                janrainScript.AppendLine("</head>");
-                janrainScript.AppendLine("<body>");
-                janrainScript.AppendLine("<a id=\"loginRef\" href=\""+JanrainSetting.ApplicationDomain+"openid/v2/signin?token_url=http%3A%2F%2Fgo.qshine.com%2FLoginAuthentication.axd%3Fmode%3Dlogin%26originate%3D" + returnUrl + "\">Redirect user to login page</a>");
-                janrainScript.AppendLine("<script type=\"text/javascript\">");
-                janrainScript.AppendLine("var rpxJsHost = ((\"https:\" == document.location.protocol) ? \"https://\" : \"http://static.\");");
-                janrainScript.AppendLine("document.write(unescape(\"%3Cscript src='\" + rpxJsHost +");
-                janrainScript.AppendLine("\"rpxnow.com/js/lib/rpx.js' type='text/javascript'%3E%3C/script%3E\"));");
-                janrainScript.AppendLine("</script>");
-                janrainScript.AppendLine("<script type=\"text/javascript\">");
-                janrainScript.AppendLine("RPXNOW.overlay = "+JanrainSetting.Overlay+";");
-                janrainScript.AppendLine("RPXNOW.language_preference = '" + JanrainSetting.LanguagePreference + "';");
-                janrainScript.AppendLine("RPXNOW.default_provider = \"" + JanrainSetting.DefaultProvider + "\";");
-                janrainScript.AppendLine("</script>");
-
-                //Send request to Janrain for user login
-                janrainScript.AppendLine("<script type=\"text/javascript\">");
-                janrainScript.AppendLine("setTimeout('loginJanRain()',1);");
-                janrainScript.AppendLine("function loginJanRain(){document.getElementById('loginRef').click();}");
-                janrainScript.AppendLine("</script>");
-                janrainScript.AppendLine("</body>");
-                janrainScript.AppendLine("</html>");
-                response.Write(janrainScript.ToString());
+                case "login":
+                    ProcessLoginRequest(context);
+                    break;
+                case "widget_embed":
+                    ProcessEmbedWidgetRequest(context);
+                    break;
+                case "widget_overlay":
+                default:
+                    ProcessDefaultWidgetRequest(context);
+                    break;
             }
         }
+
+        private void ProcessLoginRequest(HttpContext context)
+        {
+            HttpResponse response = context.Response;
+            HttpRequest request = context.Request;
+
+            string apiKey = JanrainSetting.ApiKey;
+            const string paramToken = "token";
+
+            //Check the token postback from Janrain Server
+            String returnUrl = request.QueryString["originate"];
+
+            // Get the login token passed back from the RPX authentication service
+            string loginToken = request.Form[paramToken];
+
+            if (String.IsNullOrEmpty(loginToken))
+            {
+                response.Redirect(JanrainSetting.HomeUrl);
+                return;
+            }
+
+            // Create an RPX wrapper to get the user's data
+            Rpx rpx = new Rpx(apiKey, JanrainSetting.ApplicationDomain);
+
+
+            try
+            {
+                // Get the user's details
+                XmlElement authInfo = rpx.AuthInfo(loginToken);
+
+
+                // Log the user in
+                this.loginUser(authInfo, response, returnUrl);
+            }
+            catch (SystemException ex)
+            {
+                StringBuilder errorHtml = new StringBuilder();
+                errorHtml.AppendLine("Failed to login through partner site.");
+                errorHtml.AppendLine("<span style='display:none'>" + ex.Message + "</span>");
+                response.Write(errorHtml.ToString());
+            }
+
+        }
+
+        private void ProcessDefaultWidgetRequest(HttpContext context)
+        {
+            HttpResponse response = context.Response;
+            HttpRequest request = context.Request;
+
+            //Add javascript into current login page
+            StringBuilder janrainScript = new StringBuilder();
+            String returnUrl = request.QueryString["ReturnUrl"];
+            String tokenUrl = request.Url.Scheme + ":%2F%2F" + request.Url.Authority + request.ApplicationPath +
+                "%2FLoginAuthentication.axd%3Fmode%3Dlogin%26originate%3D" + returnUrl;
+
+            //tokenUrl = "http://go.qshine.com/LoginAuthentication.axd%3Fmode%3Dlogin%26originate%3D" + returnUrl;
+            string url = JanrainSetting.ApplicationDomain + "openid/embed?token_url=" + tokenUrl;
+
+
+
+            janrainScript.AppendLine("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
+            janrainScript.AppendLine("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+            janrainScript.AppendLine("<head>");
+            janrainScript.AppendLine("</head>");
+            janrainScript.AppendLine("<body>");
+            janrainScript.AppendLine("<script type=\"text/javascript\">");
+            janrainScript.AppendLine("var rpxJsHost = ((\"https:\" == document.location.protocol) ? \"https://\" : \"http://static.\");");
+            janrainScript.AppendLine("document.write(unescape(\"%3Cscript src='\" + rpxJsHost +");
+            janrainScript.AppendLine("\"rpxnow.com/js/lib/rpx.js' type='text/javascript'%3E%3C/script%3E\"));");
+            janrainScript.AppendLine("</script>");
+            janrainScript.AppendLine("<script type=\"text/javascript\">");
+            janrainScript.AppendLine("RPXNOW.overlay = " + JanrainSetting.Overlay + ";");
+            //janrainScript.AppendLine("RPXNOW.ssl = true;");
+            janrainScript.AppendLine("RPXNOW.language_preference = '" + JanrainSetting.LanguagePreference + "';");
+            janrainScript.AppendLine("RPXNOW.default_provider = \"" + JanrainSetting.DefaultProvider + "\";");
+//            janrainScript.AppendLine(String.Format("RPXNOW.show('{0}','{1}');",tokenUrl,"golive.rpxnow.com"));//JanrainSetting.ApplicationDomain));
+
+            janrainScript.AppendLine("</script>");
+
+            janrainScript.AppendLine("<iframe src=\""+url+"\"  scrolling=\"no\" frameBorder=\"no\" style=\"width:400px;height:240px; vertical-align: middle;\" align=\"center\"></iframe>");
+
+            janrainScript.AppendLine("</body>");
+            janrainScript.AppendLine("</html>");
+            response.Write(janrainScript.ToString());
+        }
+
+
+        private void ProcessDefaultWidgetRequest1(HttpContext context)
+        {
+            HttpResponse response = context.Response;
+            HttpRequest request = context.Request;
+
+            //Add javascript into current login page
+            StringBuilder janrainScript = new StringBuilder();
+            String returnUrl = request.QueryString["ReturnUrl"];
+
+            janrainScript.AppendLine("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
+            janrainScript.AppendLine("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+            janrainScript.AppendLine("<head>");
+            janrainScript.AppendLine("</head>");
+            janrainScript.AppendLine("<body>");
+            janrainScript.AppendLine("<a id=\"loginRef\" href=\""+JanrainSetting.ApplicationDomain+"openid/v2/signin?token_url=http%3A%2F%2Fgo.qshine.com%2FLoginAuthentication.axd%3Fmode%3Dlogin%26originate%3D" + returnUrl + "\">Redirect user to login page</a>");
+            janrainScript.AppendLine("<script type=\"text/javascript\">");
+            janrainScript.AppendLine("var rpxJsHost = ((\"https:\" == document.location.protocol) ? \"https://\" : \"http://static.\");");
+            janrainScript.AppendLine("document.write(unescape(\"%3Cscript src='\" + rpxJsHost +");
+            janrainScript.AppendLine("\"rpxnow.com/js/lib/rpx.js' type='text/javascript'%3E%3C/script%3E\"));");
+            janrainScript.AppendLine("</script>");
+            janrainScript.AppendLine("<script type=\"text/javascript\">");
+            janrainScript.AppendLine("RPXNOW.overlay = "+JanrainSetting.Overlay+";");
+            janrainScript.AppendLine("RPXNOW.language_preference = '" + JanrainSetting.LanguagePreference + "';");
+            janrainScript.AppendLine("RPXNOW.default_provider = \"" + JanrainSetting.DefaultProvider + "\";");
+            janrainScript.AppendLine("</script>");
+
+            //Send request to Janrain for user login
+            janrainScript.AppendLine("<script type=\"text/javascript\">");
+            janrainScript.AppendLine("setTimeout('loginJanRain()',1);");
+            janrainScript.AppendLine("function loginJanRain(){document.getElementById('loginRef').click();}");
+            janrainScript.AppendLine("</script>");
+            janrainScript.AppendLine("</body>");
+            janrainScript.AppendLine("</html>");
+            response.Write(janrainScript.ToString());
+        }
+
+        private void ProcessEmbedWidgetRequest(HttpContext context)
+        {
+            HttpResponse response = context.Response;
+            HttpRequest request = context.Request;
+
+            string url = JanrainSetting.ApplicationDomain+"openid/embed?token_url=http%3A%2F%2Fgo.qshine.com%2FLoginAuthentication.axd%3Fmode%3Dlogin%26originate%3D";
+
+            response.Redirect(url);
+        }
+
 
         private void loginUser(XmlElement authInfo, HttpResponse response,string returnUrl)
         {
